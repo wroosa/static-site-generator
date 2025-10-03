53b8b14097f187433114423dc721d009656f8bbd
Walter R <wroosa111@gmail.com>
2025-10-03 10:50:31 -0400

    Add function to split text nodes by delimiters
    Add function to take a list of text nodes and split them by a specific delimiter representing a different text type. Current implementation should be reworked later with regex for a more robust implementation. Does not support nested delimiters but does support runs '```'

--- src/split_nodes_delimiter.py
+++ src/split_nodes_delimiter.py
@@ -1,8 +1,13 @@
 from textnode import TextType, TextNode
 
+# Helper function to skip elements of an enum
+def skip(iterator, n):
+    for _ in range(n):
+        next(iterator, None)
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
+    new_nodes = []
     for node in old_nodes:
         # Only split nodes that are text that has not already been identified
         if node.text_type == TextType.TEXT:
@@ -11,26 +16,43 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             runs = []
             start = None
             count = 0
-        for i, c in enumerate(node.text):
-            if c == delimiter:
-                if count == 0:
-                    start = i
-                count +=1
-            elif count:
-                runs.append((start, count))
-                count = 0
-        if count:
-            runs.append((start, count))
-        
-        
+            last_end = 0
 
-        
-    return runs
+            text_enum = enumerate(node.text)
+            for i, c in text_enum:
+                if c == delimiter:
+                    if count == 0:
+                        start = i
+                    count +=1
+                elif count:
+                    runs.append((start, count))
+                    close = node.text.find(count * delimiter, i + 1)
+                    if close != -1:
+
+                        pre = node.text[last_end:start]
+                        if pre != '':
+                            pre_node = TextNode(pre, TextType.TEXT)
+                            new_nodes.append(pre_node)
+
+                        match = node.text[start + count:close]
+                        match_node = TextNode(match, text_type)
+                        new_nodes.append(match_node)
+
+                        last_end = close + count
+                        skip(text_enum, close - i + count)
+
+                    count = 0
             
-                    
+
+            if last_end == 0:
+                # Add the whole node as text
+                new_nodes.append(node)
+            elif last_end < len(node.text):
+                new_nodes.append(TextNode(node.text[last_end:], TextType.TEXT))
+                                 
+        else:
+            new_nodes.append(node)
+        
+    return new_nodes
 
 
-
-test_node = TextNode("hello ``` what's up `` fellow ``` people `` asdkjash `", TextType.TEXT)
-
-print(split_nodes_delimiter([test_node], "`", TextType.CODE))
