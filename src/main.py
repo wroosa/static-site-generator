adc00b4b94828869452346e891db504df21b162b
Walter R <wroosa111@gmail.com>
2025-10-11 10:37:50 -0400

    Put the copy_dir func into a utils module
    Create a utils module and put the copy_dir func and helpers in it.

--- src/main.py
+++ src/main.py
@@ -1,70 +1,9 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode
-from pathlib import Path
-import shutil
+from utils import copy_dir
 
 def main():
    copy_dir('static', 'public')
-
-def copy_dir(from_dir, to_dir):
     
-    # Safety check to make sure the paths are in the working directory and are not files
-    working_path = Path().resolve()
-    to_path = (working_path / Path(to_dir)).resolve()
-    from_path = (working_path / Path(from_dir)).resolve()
-
-    if not path_in_working_dir(to_path):
-        raise Exception('Error: Target path is outside of the project directory')
-    
-    if not path_in_working_dir(from_path):
-        raise Exception('Error: From path is outside of the project directory')
-    
-    if to_path.is_file():
-        raise Exception("Error: Target path is a file and not a directory")
-    
-    if from_path.is_file():
-        raise Exception("Error: From path is a file and not a directory")
-    
-    # Delete all items in the target directory
-    try:
-        for item in to_path.iterdir():
-            if item.is_dir():
-                shutil.rmtree(item)
-            else:
-                item.unlink()
-    except:
-        raise Exception(f'Error deleting contents of public directory')
-
-    # Easy method
-    # from_path.copy_into(to_path)
-
-    # Recursive Method
-    copy_dir_rec(from_path, to_path)
-
-    return None
-
-
-def copy_dir_rec(from_path, to_path):
-    
-    items = from_path.iterdir()
-
-    for item in items:
-        if item.is_dir():
-            to = (to_path / item.name)
-            to.mkdir()
-            copy_dir_rec(item, to)
-        else:
-            item.copy(to_path / item.name)
-
-def path_in_working_dir(path):
-
-    working_path = Path().resolve()
-    
-    if working_path not in path.parents and working_path != path:
-        return False
-    else:
-        return True
-    
-
 if __name__ == "__main__":
     main()
