15d875fab72492e927fbe4ab0686dd4dc47c6c72
Walter R <wroosa111@gmail.com>
2025-10-13 18:26:54 -0400

    Add calls to main and server startup to main script
    Add function calls to main to create HTML page and populate the public folder. Add BeautifulSoup4 as a library to format the HTML.

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,6 +1,7 @@
 from markdown_to_html_node import markdown_to_html_node
 from extract_title import extract_title
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 def generate_page(from_path, template_path, dest_path):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
@@ -15,13 +16,14 @@ def generate_page(from_path, template_path, dest_path):
     content = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     html = template.replace('{{ Title }}', title).replace('{{ Content }}', content)
+    pretty_html = BeautifulSoup(html).prettify()
 
     # Create the HTML file and write the HTML to it
     html_file = Path(dest_path)
     html_file.parent.mkdir(parents=True, exist_ok=True)
-    html_file.write_text(html)
+    html_file.write_text(pretty_html)
 
-    
+
     
     
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from utils import copy_dir, clean_dir
+from generate_page import generate_page
 
 def main():
 
    clean_dir('public')
    copy_dir('static', 'public')
+   generate_page('content/index.md', 'template.html', 'public/index.html')
     
 if __name__ == "__main__":
     main()
